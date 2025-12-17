1c059904ff091feb34c34d8b5aa0c7d65b881c2c
neishis <vika.kondrashova.05@inbox.ru>
2025-12-17 17:16:25 +0300

    1commit

---
+++ tasks/kondrashova_v_gauss_filter_vertical_split/mpi/include/ops_mpi.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "kondrashova_v_gauss_filter_vertical_split/common/include/common.hpp"
+#include "task/include/task.hpp"
+
+namespace kondrashova_v_gauss_filter_vertical_split {
+
+class NesterovATestTaskMPI : public BaseTask {
+ public:
+  static constexpr ppc::task::TypeOfTask GetStaticTypeOfTask() {
+    return ppc::task::TypeOfTask::kMPI;
+  }
+  explicit NesterovATestTaskMPI(const InType &in);
+
+ private:
+  bool ValidationImpl() override;
+  bool PreProcessingImpl() override;
+  bool RunImpl() override;
+  bool PostProcessingImpl() override;
+};
+
+}  // namespace kondrashova_v_gauss_filter_vertical_split
